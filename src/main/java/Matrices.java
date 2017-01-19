85d6b304aed3a0e4a69229d9324dcd0b6b7df97e
Satchel Grant <grantsrb@gmail.com>
2017-01-19 14:46:02 -0800

    adjust error handling, add vector overloads of most 2d array functions with testing

--- src/main/java/Matrices.java
+++ src/main/java/Matrices.java
@@ -1,62 +1,91 @@
 import java.lang.Math;
 
 public class Matrices{
-  public static double[][] multiplyMatrix(double[][] leftMatrix, double[][] rightMatrix) {
-    double[][] product = new double[leftMatrix.length][rightMatrix[0].length];
-    try{
-      for(int rightCol = 0; rightCol < rightMatrix[0].length; rightCol++){
-        for(int row = 0; row < leftMatrix.length; row++){
-          for(int leftCol = 0; leftCol < leftMatrix[0].length; leftCol++) {
-            product[row][rightCol] += leftMatrix[row][leftCol] * rightMatrix[leftCol][rightCol];
-          }
-        }
-      }
-      return product;
-    } catch(IndexOutOfBoundsException e) {
+  public static double[][] multiply(double[][] leftMatrix, double[][] rightMatrix) {
+    if(leftMatrix[0].length != rightMatrix.length){
       return null;
     }
+    double[][] product = new double[leftMatrix.length][rightMatrix[0].length];
+    for(int rightCol = 0; rightCol < rightMatrix[0].length; rightCol++){
+      for(int row = 0; row < leftMatrix.length; row++){
+        for(int leftCol = 0; leftCol < leftMatrix[0].length; leftCol++) {
+          product[row][rightCol] += leftMatrix[row][leftCol] * rightMatrix[leftCol][rightCol];
+        }
+      }
+    }
+    return product;
   }
 
   public static boolean equals(double[][] matrix1, double[][] matrix2) {
-    try{
-      for(int row = 0; row < matrix1.length; row++){
-        for(int col = 0; col < matrix1[0].length; col++) {
-          if(Math.abs(matrix1[row][col] - matrix2[row][col]) > 0.00001d){
-            return false;
-          }
-        }
+    if(matrix1 == null || matrix2 == null){
+      if(matrix1 == matrix2){
+        return true;
+      } else {
+        return false;
       }
-      return true;
-    } catch(IndexOutOfBoundsException e) {
+    }
+    if(matrix1.length != matrix2.length || matrix1[0].length != matrix2[0].length){
       return false;
     }
-  }
-
-  public static boolean equals(double[] vector1, double[] vector2) {
-    try{
-      for(int col = 0; col < vector1.length; col++) {
-        if(Math.abs(vector1[col] - vector2[col]) > 0.00001d){
+    for(int row = 0; row < matrix1.length; row++){
+      for(int col = 0; col < matrix1[0].length; col++) {
+        if(Math.abs(matrix1[row][col] - matrix2[row][col]) > 0.00001d){
           return false;
         }
       }
-      return true;
-    } catch(IndexOutOfBoundsException e) {
+    }
+    return true;
+  }
+
+  public static boolean equals(double[] vector1, double[] vector2) {
+    if(vector1 == null || vector2 == null){
+      if(vector1 == vector2){
+        return true;
+      } else {
+        return false;
+      }
+    }
+    if(vector1.length != vector2.length){
       return false;
     }
+    for(int col = 0; col < vector1.length; col++) {
+      if(Math.abs(vector1[col] - vector2[col]) > 0.00001d){
+        return false;
+      }
+    }
+    return true;
+  }
+
+  public static double[] add(double scalar, double[] vector) {
+    double[] sum = new double[vector.length];
+    for(int i = 0; i < sum.length; i++) {
+      sum[i] = scalar + vector[i];
+    }
+    return sum;
+  }
+
+  public static double[] add(double[] vector1, double[] vector2) {
+    if(vector1.length != vector2.length){
+      return null;
+    }
+    double[] sum = new double[vector1.length];
+    for(int i = 0; i < sum.length; i++) {
+      sum[i] = vector1[i] + vector2[i];
+    }
+    return sum;
   }
 
   public static double[][] add(double[][] leftMatrix, double[][] rightMatrix) {
     double[][] sum = new double[leftMatrix.length][leftMatrix[0].length];
-    try{
-      for(int row = 0; row < sum.length; row++) {
-        for(int col = 0; col < sum[0].length; col++) {
-          sum[row][col] = leftMatrix[row][col] + rightMatrix[row][col];
-        }
-      }
-      return sum;
-    } catch(IndexOutOfBoundsException e) {
+    if(leftMatrix.length != rightMatrix.length || leftMatrix[0].length != rightMatrix[0].length){
       return null;
     }
+    for(int row = 0; row < sum.length; row++) {
+      for(int col = 0; col < sum[0].length; col++) {
+        sum[row][col] = leftMatrix[row][col] + rightMatrix[row][col];
+      }
+    }
+    return sum;
   }
 
   public static double[][] add(double scalar, double[][] matrix) {
@@ -69,28 +98,44 @@ public class Matrices{
     return sum;
   }
 
-  public static double[][] multiplyScalar(double scalar, double[][] matrix){
+  public static double[][] multiply(double scalar, double[][] matrix){
     double[][] product = new double[matrix.length][matrix[0].length];
     for(int row = 0; row < matrix.length; row++) {
-      for(int col = 0; col < matrix[0].length; col++) {
-        product[row][col] = scalar * matrix[row][col];
-      }
+      product[row] = Matrices.multiply(scalar, matrix[row]);
+    }
+    return product;
+  }
+
+  public static double[] multiply(double scalar, double[] vector){
+    double[] product = new double[vector.length];
+    for(int i = 0; i < vector.length; i++) {
+      product[i] = scalar * vector[i];
+    }
+    return product;
+  }
+
+  public static double[] multiplyPairwise(double[] vector1, double[] vector2) {
+    if(vector1.length != vector2.length){
+      return null;
+    }
+    double[] product = new double[vector1.length];
+    for(int i = 0; i < vector1.length; i++){
+      product[i] = vector1[i] * vector2[i];
     }
     return product;
   }
 
   public static double[][] multiplyPairwise(double[][] leftMatrix, double[][] rightMatrix) {
-    double[][] product = new double[leftMatrix.length][leftMatrix[0].length];
-    try{
-      for(int row = 0; row < leftMatrix.length; row++){
-        for(int col = 0; col < leftMatrix[0].length; col++) {
-          product[row][col] = leftMatrix[row][col] * rightMatrix[row][col];
-        }
-      }
-      return product;
-    } catch(IndexOutOfBoundsException e) {
+    if(leftMatrix.length != rightMatrix.length || leftMatrix[0].length != rightMatrix[0].length){
       return null;
     }
+    double[][] product = new double[leftMatrix.length][leftMatrix[0].length];
+    for(int row = 0; row < leftMatrix.length; row++){
+      for(int col = 0; col < leftMatrix[0].length; col++) {
+        product[row][col] = leftMatrix[row][col] * rightMatrix[row][col];
+      }
+    }
+    return product;
   }
 
   public static double[][] sum(int dimension, double[][] matrix) {
@@ -121,12 +166,26 @@ public class Matrices{
   }
 
   // public static double[][] reducedRowForm(double[][] matrix) {
-  //
-  //   for(int col = 0; col < matrix[0].length; col++){
-  //     for(int row = 0; row < matrix.length; row++) {
-  //       double ratio = matrix[col][]
+  //   int skippedColumns = 0;
+  //   double[][] reducedMatrix = Matrices.deepCopy(matrix);
+  //   for(int col = 0; col < reducedMatrix[0].length; col++){
+  //     if(col < reducedMatrix.length) {
+  //       int pivotIndex = Matrices.firstNonZeroIndex(Matrices.column(col, reducedMatrix));
+  //       if(pivotIndex > -1){
+  //         reducedMatrix = Matrices.swapRows(col-skippedColumns, pivotIndex, reducedMatrix);
+  //         for(int row = col-skippedColumns+1; row < reducedMatrix.length; row++) {
+  //           double ratio = reducedMatrix[col-skippedColumns][col]/reducedMatrix[row][col];
+  //           double[] reductionRow = Matrices.multiply(-ratio, reducedMatrix[col-skippedColumns][col]);
+  //           reducedMatrix[row] = Matrices.add(reductionRow, reducedMatrix[row]);
+  //         }
+  //       } else {
+  //         skippedColumns++;
+  //       }
+  //     } else {
+  //       break;
   //     }
   //   }
+  //   return reducedMatrix;
   // }
 
   public static int maxIndex(double[] vector){
--- src/test/java/MatricesTest.java
+++ src/test/java/MatricesTest.java
@@ -11,6 +11,13 @@ public class MatricesTest{
   }
 
   @Test
+  public void equals_comparesNullForEquality_boolean(){
+    double[][] matrix1 = null;
+    double[][] matrix2 = null;
+    assertTrue(Matrices.equals(matrix1, matrix2));
+  }
+
+  @Test
   public void equals_comparesVectorsForEquality_boolean(){
     double[] vector1 = new double[] {1,0,1};
     double[] vector2 = new double[] {1,0,1};
@@ -18,11 +25,28 @@ public class MatricesTest{
   }
 
   @Test
-  public void multiplyMatrix_standardMatrixMultiplication_2DdoubleArray(){
+  public void equals_comparesNullForEqualityVectors_boolean(){
+    double[] vector1 = null;
+    double[] vector2 = null;
+    assertTrue(Matrices.equals(vector1, vector2));
+  }
+
+  @Test
+  public void multiply_standardMatrixMultiplication_2DdoubleArray(){
     double[][] leftMatrix = new double[][] {{1,2,3},{4,5,6},{7,8,9}};
     double[][] rightMatrix = new double[][] {{10,11,12,13,14},{15,16,17,18,19},{20,21,22,23,24}};
     double[][] knownProduct = new double[][] {{100,106,112,118,124},{235,250,265,280,295},{370,394,418,442,466}};
-    double[][] testProduct = Matrices.multiplyMatrix(leftMatrix, rightMatrix);
+    double[][] testProduct = Matrices.multiply(leftMatrix, rightMatrix);
+    assertTrue(Matrices.equals(knownProduct, testProduct));
+  }
+
+  @Test
+  public void multiply_standardMatrixMultiplication_null(){
+    double[][] leftMatrix = new double[][] {{1,2,3},{4,5,6},{7,8,9}};
+    double[][] rightMatrix = new double[][] {{10,11,12,13,14},{15,16,17,18,19},{20,21,22,23,24},{25,26,27,28,29}};
+    double[][] knownProduct = null;
+    double[][] testProduct = Matrices.multiply(leftMatrix, rightMatrix);
+    System.out.println(testProduct == null);
     assertTrue(Matrices.equals(knownProduct, testProduct));
   }
 
@@ -40,16 +64,52 @@ public class MatricesTest{
     double[][] testMatrix = new double[][] {{2,1,2}, {0,2,-1}, {2,2,3}};
     double[][] knownSum = new double[][] {{7,6,7}, {5,7,4}, {7,7,8}};
     double scalar = 5;
-    double [][] testSum = Matrices.add(scalar,testMatrix);
+    double[][] testSum = Matrices.add(scalar,testMatrix);
     assertTrue(Matrices.equals(knownSum, testSum));
   }
 
   @Test
-  public void multiplyScalar_scalarMultiplicationAcrossMatrix_2DdoubleArray(){
+  public void add_scalarAdditionAcrossVector_doubleArray(){
+    double[] testVector = new double[] {1,2,3};
+    double scalar = 5;
+    double[] knownSum = new double[] {6,7,8};
+    double[] testSum = Matrices.add(scalar,testVector);
+    assertTrue(Matrices.equals(knownSum, testSum));
+  }
+
+  @Test
+  public void add_standardVectorAddition_doubleArray(){
+    double[] vector1 = new double[] {1,2,3};
+    double[] vector2 = new double[] {4,5,6};
+    double[] knownSum = new double[] {5,7,9};
+    double[] testSum = Matrices.add(vector1,vector2);
+    assertTrue(Matrices.equals(knownSum, testSum));
+  }
+
+  @Test
+  public void multiply_scalarMultiplicationAcrossMatrix_2DdoubleArray(){
     double[][] testMatrix = new double[][] {{2,1,2}, {0,2,-1}, {2,2,3}};
     double[][] knownProduct = new double[][] {{10,5,10}, {0,10,-5}, {10,10,15}};
     double scalar = 5;
-    double [][] testProduct = Matrices.multiplyScalar(scalar,testMatrix);
+    double [][] testProduct = Matrices.multiply(scalar,testMatrix);
+    assertTrue(Matrices.equals(knownProduct, testProduct));
+  }
+
+  @Test
+  public void multiply_scalarMultiplicationAcrossVector_doubleArray(){
+    double[] testVector = new double[] {1,2,3};
+    double scalar = 5;
+    double[] knownProduct = new double[] {5,10,15};
+    double[] testProduct = Matrices.multiply(scalar,testVector);
+    assertTrue(Matrices.equals(knownProduct, testProduct));
+  }
+
+  @Test
+  public void multiplyPairwise_multipliesVectorsElementByElement_doubleArray(){
+    double[] vector1 = new double[] {1,2,3};
+    double[] vector2 = new double[] {4,5,6};
+    double[] knownProduct = new double[] {4,10,18};
+    double[] testProduct = Matrices.multiplyPairwise(vector1, vector2);
     assertTrue(Matrices.equals(knownProduct, testProduct));
   }
 
